20d58b676284e68d6d131c9d9b61df02b56abd69
Matthew Bensley <mbensley@gmail.com>
2022-12-28 21:26:34 -0500

    Clean up AoC 2022-01 and add additional heapq solution.

--- 2022-01/aoc-2022-01.py
+++ 2022-01/aoc-2022-01.py
@@ -2,21 +2,17 @@
 # blank line in between each elf
 # who has the most calories
 import os
+import heapq
 
 
-def count_top_n_calories(cal_list, top_n):
-    total_cal_list = sorted([sum(elf_list) for elf_list in cal_list])
-    return sum(total_cal_list[-top_n:])
+with open(os.getcwd() + '/2022-01/input.txt') as f:
+    cal_list = []
+    for pack in f.read().split('\n\n'):
+        cal_list.append(sum([int(item) for item in pack.split('\n')]))
+    cal_list.sort()
 
+    print('Part A: %i' % sum(cal_list[-1:]))
+    print('Part B: %i' % sum(cal_list[-3:]))
 
-def build_cal_list():
-    with open(os.getcwd() + '/2022-01/input.txt') as f:
-        cal_list = []
-        for pack in f.read().split('\n\n'):
-            cal_list.append([int(item) for item in pack.split('\n')])
-        return cal_list
-
-
-cal_list = build_cal_list()
-print('Part A: %i' % count_top_n_calories(cal_list, 1))
-print('Part B: %i' % count_top_n_calories(cal_list, 3))
+    print('Part A: %i' % heapq.nlargest(1, cal_list).pop())
+    print('Part B: %i' % sum(heapq.nlargest(3, cal_list)))
