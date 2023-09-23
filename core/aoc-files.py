ad2bc2b32eba8ff15d2112be9235a886b32eae09
Matthew Bensley <mbensley@gmail.com>
2023-09-23 09:46:35 -0400

    Update template to optionally read test data from a file or code.

---
+++ core/aoc-files.py
@@ -0,0 +1,9 @@
+def inputfile():
+    year_dir = '2015'
+    filename = os.path.join(year_dir, 'input.txt')
+    return os.path.join(os.getcwd(), filename)
+
+
+def getinput(f, test):
+    test_input = ['2x3x4', '1x1x10']
+    return test_input if test else f.read()
