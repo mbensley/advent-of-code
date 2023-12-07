cae5432197a80f5e56d5f32fd6df96e1ddafd88a
Matthew Bensley <mbensley@gmail.com>
2023-12-07 14:26:42 -0500

    AoC 2023 Day 7 more compact solution using Counter

--- 2023/aoc2023-07.py
+++ 2023/aoc2023-07.py
@@ -1,5 +1,6 @@
 # Timings: Part A: 10:00 / Part B: Fun Run
 import os
+from collections import Counter
 
 def inputfile():
     return os.path.join(os.path.dirname(__file__), 'input.txt')
@@ -8,78 +9,48 @@ def getinput(f, test=False):
     test_input = []
     return test_input if test else f.read().splitlines()
 
-def sortcards(cardlist):
-    def gethand(hand):
-        hset = list(set(hand))
-        if len(hset) == 5:
-            return 'H'
-        if len(hset) == 4:
-            return 'O'
-        if len(hset) == 1:
-            return '5'
-        if len(hset) == 3: #2 pair, 3 of a kind
-            # count each card
-            if hand.count(hset[0]) == 3 or hand.count(hset[1]) == 3 or hand.count(hset[2]) == 3:
-                return '3'
-            return 'T'
-        if len(hset) == 2: #4 of a kind of FullHouse
-            if hand.count(hset[0]) == 4 or hand.count(hset[1]) == 4:
-                return '4'
+def gethand(hand):
+    ctr = Counter(hand)
+    mc = ctr.most_common()
+    if mc[0][1] == 5: return '5'
+    if mc[0][1] == 4: return '4'
+    if mc[0][1] == 3:
+        if mc[1][1] == 2:
             return 'F'
-
+        return '3'
+    if ctr.most_common()[0][1] == 2:
+        if mc[1][1] == 2:
+            return 'T'
+        return 'O'
+    if ctr.most_common()[0][1] == 1:
+        return 'H'
+        
+def sortcards(cardlist):
     cardorder = '23456789TJQKA'
     cardlist.sort(key=lambda t: [cardorder.index(c) for c in t[0]])
     handorder = 'HOT3F45'
     cardlist.sort(key=lambda t: handorder.index(gethand(t[0])))
 
 def sortcardsb(cardlist):
-    def gethand(hand):
-        hset = list(set(hand))
-        if 'J' not in hset:
-            if len(hset) == 5:
-                return 'H'
-            if len(hset) == 4:
-                return 'O'
-            if len(hset) == 1:
-                return '5'
-            if len(hset) == 3: #2 pair, 3 of a kind
-                # count each card
-                if hand.count(hset[0]) == 3 or hand.count(hset[1]) == 3 or hand.count(hset[2]) == 3:
-                    return '3'
-                return 'T'
-            if len(hset) == 2: #4 of a kind of FullHouse
-                if hand.count(hset[0]) == 4 or hand.count(hset[1]) == 4:
-                    return '4'
-                return 'F'
+    def gethandb(hand):
+        ctr = Counter(hand)
+        if 'J' not in ctr:
+            return gethand(hand)
         else: # Use the Joker to make a stronger hand 
+            # Replace each joker with the most common card.
             jcount = hand.count('J')
-            hset.remove('J')
-            print(len(hset), jcount, hset)
-            if len(hset) == 1 or len(hset) == 0:
-                return '5'
-            if jcount == 3:
-                return '4'
-            if jcount == 2:
-                #hset is 3 or 2
-                if len(hset) == 3:
-                    return '3'
-                return '4'
-            if jcount == 1:
-                #hset is 4, 3, or 2
-                if len(hset) == 4:
-                    return 'O'
-                if len(hset) == 3:
-                    return '3'
-                # otherwise it's a 2-2 or 3-1 card split
-                if hand.count(hset[0]) == 2:
-                    return 'F'
-                return '4'
-
+            if jcount == 5: return '5'
+            del ctr['J']
+            ctr[ctr.most_common(1)[0][0]] += jcount
+            nhand = ''
+            for e,c in ctr.items():
+                nhand += ''.join([e*c])
+            return gethand(nhand)
 
     cardorder = 'J23456789TQKA'
     cardlist.sort(key=lambda t: [cardorder.index(c) for c in t[0]])
     handorder = 'HOT3F45'
-    cardlist.sort(key=lambda t: handorder.index(gethand(t[0])))
+    cardlist.sort(key=lambda t: handorder.index(gethandb(t[0])))
 
 with open(inputfile()) as f:
     input = getinput(f)
