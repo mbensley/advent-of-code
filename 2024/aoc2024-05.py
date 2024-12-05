9bf275005310f370053afc8ec731b6b343611864
Matthew Bensley <mbensley@gmail.com>
2024-12-05 00:35:52 -0500

    AoC 2024 Day 5 solution.

---
+++ 2024/aoc2024-05.py
@@ -0,0 +1,51 @@
+import os
+
+def inputfile():
+    return os.path.join(os.path.dirname(__file__), 'input.txt')
+
+def getinput(f, test=False):
+    test_input = ['2x3x4', '1x1x10']
+    return test_input if test else f.read()
+
+def is_valid(rules, update):
+    for r0, r1 in rules:
+        if r0 in update and r1 in update:
+            if update.index(r0) > update.index(r1):
+                return False, (r0,r1)
+    return True, None
+
+def sum_valid_mids(rules, updates):
+    total = 0
+    for update in updates:
+        update = update.split(',')
+        if is_valid(rules, update)[0]:
+            total += int(update[len(update)//2])
+    return total
+
+
+def sum_invalid_mids(rules, updates):
+    total = 0
+    for update in updates:
+        update = update.split(',')
+        valid, rule = is_valid(rules, update)
+        if not valid:
+            while not valid:
+                i0 = update.index(rule[0])
+                i1 = update.index(rule[1])
+                update[i0] = rule[1]
+                update[i1] = rule[0]
+                valid, rule = is_valid(rules, update)
+            total += int(update[len(update)//2])
+    return total
+
+def build_rules(rules):
+    rule_list = []
+    for rule in rules:
+        rule_list.append(rule.split('|'))
+    return rule_list
+
+with open(inputfile()) as f:
+    input = getinput(f)
+    rules, updates = input.split('\n\n')
+    print('Part A: %i' % sum_valid_mids(build_rules(rules.splitlines()), updates.splitlines()))
+    print('Part B: %i' % sum_invalid_mids(build_rules(rules.splitlines()), updates.splitlines()))
