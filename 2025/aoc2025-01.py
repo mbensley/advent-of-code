f00ce2c43c943fe3f630f742b0a17317916bbef1
Matthew Bensley <mbensley@gmail.com>
2025-12-01 12:56:53 -0500

    AOC 2025 Day 1 initial solution

--- 2025/aoc2025-01.py
+++ 2025/aoc2025-01.py
@@ -1,20 +1,5 @@
 # Timings: Part A: XX:00 / Part B: XX:00
-from collections import defaultdict
-from collections import deque
-import ast
-import hashlib
-import heapq
-import math
 import os
-import queue
-import re
-import string
-import sys
-from itertools import product
-from itertools import combinations
-from functools import cache
-from math import floor
-from copy import deepcopy
 from typing import TextIO
 
 # https://docs.python.org/3/library/
@@ -24,12 +9,33 @@ def inputfile():
     return os.path.join(os.path.dirname(__file__), 'input.txt')
 
 def getinput(f: TextIO, test: bool = True) -> list[str]:
-    test_input = ['2x3x4', '1x1x10']
+    test_input = ['R11', 'L61', 'L1', 'R1']
     return test_input if test else f.read().splitlines()
 
+def turn_dial(start: int, steps: int, direction: str) -> int:
+    if direction == 'R':
+        return (start+steps) % 100
+    return (start-steps) % 100
 
 with open(inputfile()) as f:
     input = getinput(f)
 
-    print('Part A: %i' % 0)
-    print('Part B: %i' % 1)
+    num_zeros_a = 0
+    num_zeros_b = 0
+    cur = 50
+    for l in input:
+        dir = l[0]
+        steps = int(l[1:])
+        new_cur = turn_dial(cur, steps, dir)
+        if new_cur == 0:
+            num_zeros_a += 1
+            num_zeros_b += 1
+        num_zeros_b += (steps // 100)
+        if dir == 'R' and new_cur < cur and new_cur != 0 and cur != 0:
+            num_zeros_b += 1
+        if dir == 'L' and new_cur > cur and new_cur != 0 and cur != 0:
+            num_zeros_b += 1
+        cur = new_cur
+
+    print('Part A: %i' % num_zeros_a)
+    print('Part B: %i' % num_zeros_b)
